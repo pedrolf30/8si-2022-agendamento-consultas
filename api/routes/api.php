26fa94b7d7df50563f507fe0f61b02ed66796b8a
pedrolf30 <pedrolf30@gmail.com>
2022-10-24 23:13:58 -0300

    adding: user register, login and auth

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PessoaController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('pessoas', PessoaController::class);
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('pessoas', PessoaController::class);
+});
