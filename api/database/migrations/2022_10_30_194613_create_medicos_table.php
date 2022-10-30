c7020a50e571c5807bdba22852295f47ec58bb03
pedrolf30 <pedrolf30@gmail.com>
2022-10-30 16:49:43 -0300

    added: create migration

---
+++ api/database/migrations/2022_10_30_194613_create_medicos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('especialidade');
+            $table->string('crm');
+            $table->unsignedBigInteger('pessoa_id');
+            $table->foreign('pessoa_id')->references('id')->on('pessoas');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
